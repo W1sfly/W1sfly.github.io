9c1dc5c36949fc5262dd912c2864b01493b95fe6
#W1sfly# <86053823+W1sfly@users.noreply.github.com>
2021-09-09 15:12:22 +0000

    Add files via upload

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "Script systemSO",
+        "excerpt":"   Función   El siguiente script nos dará si una máquina está activa o no y su sistema operativo, es un script bastante sencillo pero le he aplicado controles de errores para que funcione perfectamente.   Utilización   ./systemSO.sh IP   Descarga   https://github.com/W1sfly/systemSO.git  ","categories": ["script","Herramienta"],
+        "tags": ["bash"],
+        "url": "http://localhost:4000/script_systemSO/",
+        "teaser":null}]
